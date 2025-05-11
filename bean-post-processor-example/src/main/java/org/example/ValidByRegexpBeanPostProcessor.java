605075a32ee1e1d4dc956fb22a39d0f1b060a926
Курнышов Илья Михайлович <IMKurnyshov@get-lab.ru>
2025-05-11 20:40:39 +0300

    bean-post-processor-example ValidByRegexpBeanPostProcessor basic refactor

--- bean-post-processor-example/src/main/java/org/example/ValidByRegexpBeanPostProcessor.java
+++ bean-post-processor-example/src/main/java/org/example/ValidByRegexpBeanPostProcessor.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Parameter;
 import java.lang.reflect.Proxy;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,42 +27,87 @@ public class ValidByRegexpBeanPostProcessor implements BeanPostProcessor {
 
     Class clazz = bean.getClass();
 
-    for (Method method : clazz.getMethods()) {
+    fillMethodWithMarkedParametersMap(clazz.getMethods(), methodsWithMarkedParameters);
+
+    if (!methodsWithMarkedParameters.isEmpty()) {
+      return generateProxyWithValidation(bean, methodsWithMarkedParameters, clazz);
+    }
+
+    return bean;
+  }
+
+  private void fillMethodWithMarkedParametersMap(Method[] methods,
+      Map<String, List<ParamNumberWithRegexp>> methodsWithMarkedParameters) {
+    for (Method method : methods) {
       List<ParamNumberWithRegexp> markedParameters = new ArrayList<>();
-      for (Parameter parameter : method.getParameters()) {
-        if (parameter.isAnnotationPresent(ValidByRegexp.class)) {
-          int parameterNumber = Integer.parseInt(
-              parameter.getName().replaceAll("arg(?<counter>\\d)", "${counter}"));
-          markedParameters.add(
-              ParamNumberWithRegexp.builder()
-                  .paramNumber(parameterNumber)
-                  .regexp(parameter.getAnnotation(ValidByRegexp.class).regexp())
-                  .build());
-        }
-      }
+
+      checkParametersForAnnotationAndAddToMarkedParametersIfNeeds(method, markedParameters);
+
       if (!markedParameters.isEmpty()) {
         methodsWithMarkedParameters.put(method.getName(), markedParameters);
       }
     }
+  }
 
-    if (!methodsWithMarkedParameters.isEmpty()) {
-      return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(),
-          (proxy, method, args) -> {
-            if (methodsWithMarkedParameters.containsKey(method.getName())) {
-              for (ParamNumberWithRegexp paramNumberWithRegexp : methodsWithMarkedParameters.get(method.getName())) {
-                Object arg = args[paramNumberWithRegexp.paramNumber];
-                if (!arg.toString().matches(paramNumberWithRegexp.getRegexp())) {
-                  throw new RuntimeException("param №%s with value=%s in method %s don't matches regexp '%s'"
-                      .formatted(paramNumberWithRegexp.paramNumber, arg, method.getName(), paramNumberWithRegexp.getRegexp()));
-                }
-              }
-            }
-
-            return method.invoke(bean, args);
-          });
+  private void checkParametersForAnnotationAndAddToMarkedParametersIfNeeds(
+      Method method, List<ParamNumberWithRegexp> markedParameters) {
+    for (Parameter parameter : method.getParameters()) {
+      if (parameter.isAnnotationPresent(ValidByRegexp.class)) {
+        addToMarkedParameters(parameter, markedParameters);
+      }
     }
+  }
 
-    return bean;
+  private void addToMarkedParameters(Parameter parameter,
+      List<ParamNumberWithRegexp> markedParameters) {
+    markedParameters.add(
+        ParamNumberWithRegexp
+            .builder()
+            .paramNumber(extractParameterNumber(parameter))
+            .regexp(extractRegexp(parameter))
+            .build());
+  }
+
+  private int extractParameterNumber(Parameter parameter) {
+    return Integer.parseInt(
+        parameter.getName()
+            .replaceAll("arg(?<counter>\\d)", "${counter}"));
+  }
+
+  private String extractRegexp(Parameter parameter) {
+    return parameter.getAnnotation(ValidByRegexp.class).regexp();
+  }
+
+  private Object generateProxyWithValidation(Object bean,
+      Map<String, List<ParamNumberWithRegexp>> methodsWithMarkedParameters, Class clazz) {
+    return Proxy.newProxyInstance(clazz.getClassLoader(), clazz.getInterfaces(),
+        (proxy, method, args) -> {
+          String methodName = method.getName();
+
+          if (methodsWithMarkedParameters.containsKey(methodName)) {
+            validArguments(methodsWithMarkedParameters, method, args, methodName);
+          }
+
+          return method.invoke(bean, args);
+        });
+  }
+
+  private void validArguments(
+      Map<String, List<ParamNumberWithRegexp>> methodsWithMarkedParameters, Method method,
+      Object[] args, String methodName) {
+    for (ParamNumberWithRegexp paramNumWithRegexp : methodsWithMarkedParameters.get(methodName)) {
+      validArgByRegexp(method, paramNumWithRegexp, args[paramNumWithRegexp.paramNumber]);
+    }
+  }
+
+  private void validArgByRegexp(Method method,
+      ParamNumberWithRegexp paramNumberWithRegexp, Object arg) {
+    if (!arg.toString().matches(paramNumberWithRegexp.getRegexp())) {
+      throw new RuntimeException(
+          "param №%s with value=%s in method %s don't matches regexp '%s'"
+              .formatted(paramNumberWithRegexp.paramNumber, arg, method.getName(),
+                  paramNumberWithRegexp.getRegexp()));
+    }
   }
 
 
@@ -71,6 +115,7 @@ public class ValidByRegexpBeanPostProcessor implements BeanPostProcessor {
   @AllArgsConstructor
   @Getter
   private static class ParamNumberWithRegexp {
+
     private int paramNumber;
     private String regexp;
   }
